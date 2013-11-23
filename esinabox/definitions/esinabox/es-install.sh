89182e402aa475eb03cd77e8e27fcec847b325f1
Andrew Glover <ajglover@gmail.com>
2013-11-23 14:55:12 -0500

    minor updates

--- esinabox/definitions/esinabox/es-install.sh
+++ esinabox/definitions/esinabox/es-install.sh
@@ -1,7 +1,7 @@
 apt-get install curl -y 
 # start Java 7 64 bit
 wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java7_64.sh && bash equip_java7_64.sh
-# Elastic search (https://gist.github.com/wingdspur/2026107)
+# ElasticSearch (https://gist.github.com/wingdspur/2026107)
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
 sudo dpkg -i elasticsearch-0.90.7.deb
 # Remove items used for building, since they aren't needed anymore
